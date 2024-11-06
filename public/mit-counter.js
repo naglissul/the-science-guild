391c28d796389e07883f1f7446e2742eec80848a
naglissul <naglis1134@gmail.com>
2024-11-06 03:59:47 +0100

    Fix counter

--- public/mit-counter.js
+++ public/mit-counter.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://letscountapi.com";
-const NAMESPACE = "the-science-guild-counter";
+const NAMESPACE = "the-science-guild-counter2";
 
 async function getCounterValue(key) {
   try {
@@ -10,6 +10,10 @@ async function getCounterValue(key) {
       throw new Error(`Error: ${response.status}`);
     }
     const data = await response.json();
+    if (data.exists === "false") {
+      initCounter(key);
+      return;
+    }
     if (key === "mit-1") {
       document.getElementById("mit-1-count").innerText = data.current_value;
     } else if (key === "mit-2") {
@@ -49,6 +53,27 @@ async function incrementCounter(key) {
     console.error("Failed to increment counter:", error);
   }
 }
+
+async function initCounter(key) {
+  try {
+    const response = await fetch(`${BASE_URL}/${NAMESPACE}/${key}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data.already_exists === "false") {
+      getCounterValue(key);
+    }
+  } catch (error) {
+    console.error("Failed to increment counter:", error);
+  }
+}
+
 getCounterValue("mit-1");
 getCounterValue("mit-2");
 getCounterValue("mit-3");
